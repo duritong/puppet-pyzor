5f44e8b681fa7ea206b1d9399c3741485cd32462
mh <mh@immerda.ch>
2013-09-22 18:13:09 +0200

    open ports for pyzor

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
 # manifests/init.pp - module to manage pyzor
-class pyzor {
+class pyzor(
+  $use_shorewall = false
+) {
   include pyzor::base
+
+  if $use_shorewall {
+    include shorewall::rules::out::pyzor
+  }
 }
